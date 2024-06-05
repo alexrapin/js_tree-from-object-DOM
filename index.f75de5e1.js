e6217245cf49966d8b0a47f930020ce2db9e554b
Olexandr Rapin <alexrapin04@gmail.com>
2024-06-05 12:48:50 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,33 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    Object.entries(data).forEach(([key, value])=>{
+        const li = document.createElement("li");
+        li.textContent = key;
+        element.append(li);
+        if (Object.entries(value).length > 0) {
+            const ul = document.createElement("ul");
+            li.append(ul);
+            createTree(ul, value);
+        }
+    });
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
